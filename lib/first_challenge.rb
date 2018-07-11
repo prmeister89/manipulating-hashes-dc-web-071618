25fb3241d118e61f02b440c33ce1e3f538043ec9
Peter Meister <prmeister89@gmail.com>
2018-07-11 16:03:05 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -13,10 +13,9 @@ def first_challenge
     }
   }
 
-  #your code here
-
-
-  #remember to return your newly altered contacts hash!
+ contacts.each do |person, data|
+   data.each do |attribute, value|
+     
   contacts
 end
 
