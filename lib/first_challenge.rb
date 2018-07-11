46042da50976e71e6d266e7906321bcddb783179
Peter Meister <prmeister89@gmail.com>
2018-07-11 16:14:53 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -13,15 +13,15 @@ def first_challenge
     }
   }
 
-  contacts.each do |person, data|
-    data.each do |attribute, value|
-      if attribute == :favorite_icecream_flavors
-        value.delete_if do |flavor|
-          flavor == "strawberry"
-        end
+contacts.each do |person, data|
+  data.each do |attribute, value|
+    if attribute == :favorite_icecream_flavors
+      value.delete_if do |flavor|
+        flavor == "strawberry"
       end
     end
   end
-  contacts
+
+contacts
 end
 
